5ed17ec3e75d9f181cc5b98fe0dd89f5f8527f3b
Giuseppe31-s <giuseppe31tinti@gmail.com>
2024-03-14 12:42:28 -0300

    add functions for transformation and simulation

--- src/transform.py
+++ src/transform.py
@@ -2,10 +2,17 @@ import pandas as pd
 from datetime import datetime
 
 
-def transfrom_date(dataframe: pd.DataFrame, name_colum_date: str) -> pd.DataFrame:
+def transfrom_date_timestemp(dataframe: pd.DataFrame, name_colum_date: str) -> pd.DataFrame:
     data = dataframe.copy()
     data[f"{name_colum_date}"] = data[f"{name_colum_date}"].apply(
         lambda x: datetime.utcfromtimestamp(x)
+    )
+    return data
+
+def transfrom_date_str(dataframe: pd.DataFrame, name_colum_date: str,format: str) -> pd.DataFrame:
+    data = dataframe.copy()
+    data[f"{name_colum_date}"] = data[f"{name_colum_date}"].apply(
+        lambda x: datetime.strptime(x,format)
     )
     return data
 
